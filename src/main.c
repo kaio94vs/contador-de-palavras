9c4bcde8c7e449b2446e44982b8810f427a55ece
Kaio Vieira Dos Santos - 156072 <ra156072@le20-4.grad.fee.unicamp.br>
2017-12-01 10:48:44 -0200

    programa finalizado

--- src/main.c
+++ src/main.c
@@ -1,55 +1,46 @@
 
-/* Kaio Vieira dos Santos
- * RA: 156072
- *	Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
-*/
-
 #include <stdio.h>
+ 
+#define STRSIZE 200
+ 
+int main()
+{
+	char frase[ STRSIZE ];
+	int contador=1, i;
 
-int main() 
-{	
-	int contador_de_palavras = 1; //contador de palavras	
-	int flag = 0; //flag para simbolos e espaços
-	char ultimo_caracter, caracter;
+	int flag_simbolo=0;
+	char ultimo_caracter;
+	
+ 
+	fgets( frase, STRSIZE, stdin );
 	
 	
-	
-    while (1) //ler caracter por caracter ateh que saia do ciclo infinito
+	for (i=0; frase[i]!='\0' ; i++)
 	{
-	  scanf("%c", &caracter);
-		
-	  //verifica se acabou a frase
-	  if (caracter == '\n')
+	  
+	  if (frase[i] == '\n')
 	    {
-		printf("%d\n", contador_de_palavras);
+		printf("%d\n", contador);
 		return 0;
 	    }
-		//verifica se eh um simbolo da tabela ASCII
-	  if (caracter > 31 && caracter < 48)
-	    { 
-   		flag = 1;
-	     }
-		
-	  //verificar se é uma palavra para contagem
-	  else 
-	    if (flag == 1 && (!((caracter > 47 && caracter < 58) && (ultimo_caracter == 44 || ultimo_caracter == 46))))
-	      {
-		contador_de_palavras = contador_de_palavras + 1;
-		flag = 0;
-	      }
-		
-	  //para nao contar os decimais mais de uma vez
+	  
+	  if (frase[i] >=32 && frase[i] <= 47)
+	    flag_simbolo=1;
+	  
+	  else if (flag_simbolo==1 && (!((frase[i]>= 48 && frase[i]<= 57) && (ultimo_caracter==44 || ultimo_caracter == 46 ))))
+	  {
+	    contador++;
+	    flag_simbolo=0;
+	  }
+	  
 	  else
-	    {
-		flag = 0;
-	    }
-	
-	
-	ultimo_caracter = caracter;	 
+	    flag_simbolo=0;
+	  
+	  ultimo_caracter=frase[i];
 	}
-}
+	
 
+	
+ 
+	return 0;
+}
